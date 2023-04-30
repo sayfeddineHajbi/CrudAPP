9b89bf01135c72b8d7638c6966c33bedde27c6e0
sayfeddineHajbi <sayfhajbi@gmail.com>
2023-04-30 22:08:45 +0100

    Ajoutez des fichiers projet.

---
+++ Crud APP/sql/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO clients (name, email, phone, address)
+VALUES
+('Sayfeddine Hajbi', 'sayfhajbi@gmail.com', '+123456789', 'New York, USA'),
+('Elon Musk', 'elon.musk@spacex.com', '+111222333', 'Florida, USA'),
+('Will Smith', 'will.smith@gmail.com', '+111333555', 'California, USA'),
+('Bob Marley', 'bob@gmail.com', '+111555999', 'Texas, USA'),
+('Cristiano Ronaldo', 'cristiano.ronaldo@gmail.com', '+32447788993', 'Manchester, England'),
+('Boris Johnson', 'boris.johnson@gmail.com', '+4499778855', 'London, England');
